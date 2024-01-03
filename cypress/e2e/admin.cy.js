126fda897fc808ca2205fca08707d36a96510b4a
Lena k <ktyecz09@mail.ru>
2024-01-03 20:18:43 +0300

    first commit

--- cypress/e2e/admin.cy.js
+++ cypress/e2e/admin.cy.js
@@ -4,8 +4,11 @@ const store = require("../fixtures/store.json");
 it("Should successfully login admin", () => {
   cy.visit("http://qamid.tmweb.ru/admin"); 
   cy.login("qamid@qamid.ru" , "qamid");
+  cy.contains(store.deskForEqual).should("be.visible");
+  cy.get('[data-film-id="119"] > .conf-step__movie-title ').then(($el) => $el.textContent).should('have.text','Унесенные ветром.');
+  cy.get('[data-film-id="119"] ').invoke('text').then((text) => {
   cy.visit("qamid.tmweb.ru");
-  cy.get(selector.chooseFilms).then(($el) => $el.textContent).should('have.text','Унесенные ветром.');
+  cy.get(selector.chooseFilms).should('have.text', 'Унесенные ветром.');
   cy.get(selector.chooseTimeSession).click();
   cy.get(selector.chooseFilm).contains(store.timeSession).click();
   cy.contains(store.timeSessionForEqual).should("be.visible");
@@ -14,4 +17,5 @@ it("Should successfully login admin", () => {
   cy.url().should('include', '/client/payment.php')
   cy.contains(selector.Buttonbooking).click();
   cy.contains(store.takeTicket).should("be.visible");
+});
 });
